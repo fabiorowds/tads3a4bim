3c30b2fa823dcdc0aba437088294afe869ddcbca
fabiorowds <fabiorowds@gmail.com>
2015-10-21 20:43:33 -0200

    Criada as classes para o Topic e o Servlet não funcionará pois utiliza o JMS 2.0, que não está instalado no meu eclipse.

---
+++ ExJMS/src/main/java/br/univel/Professor.java
@@ -0,0 +1,22 @@
+package br.univel;
+
+import java.io.Serializable;
+
+public class Professor implements Serializable{
+
+	private Long matricula;
+	private String nome;
+	public Long getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
