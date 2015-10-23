de44ffa2b7791867644cd163a9d199922633f779
fabiorowds <fabiorowds@gmail.com>
2015-10-22 22:05:07 -0200

    Criação da interface WEB para utilização da classe injetada e do arquivo WEB.xml para configuração da pagina inicial.

--- ExCDI/ExCDI/src/test/java/exercise2/ItemProcessor.java
+++ ExCDI/ExCDI/src/test/java/exercise2/ItemProcessor.java
@@ -3,12 +3,14 @@ package exercise2;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
 public class ItemProcessor {
 
+	@Inject
 	private ItemDao itemDao;
 
     public void execute() {
